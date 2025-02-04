668af24d85ce9978b741775f7b167f4b65213f85
LuigiDotSRC <luigi.terashima@gmail.com>
2025-02-03 19:10:11 -0500

    add images to root page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row" id="hero">
       <div className="flex-[0_0_30%]">
         <div className="relative my-2">
           <h1 className="text-[4rem] font-bold text-transparent bg-gradient-to-r from-[#EE0979] to-[#FF6A00] bg-clip-text">
@@ -15,10 +15,27 @@ export default function Home() {
         <Button className="font-bold bg-gradient-to-r from-[#EE0979] to-[#FF6A00] hover:scale-105 transition-transform duration-[2000ms]">
           Join Now
         </Button>
-
       </div>
       <div className="flex-[0_0_70%]">
-        <p>test</p>
+        <div className="flex flex-row">
+          <div className="flex flex-col items-end">
+            <img 
+              src="/luisa-brimble-aFzg83dvnAI-unsplash.jpg" 
+              alt="People sharing a meal on a table"
+              className="mx-2 rounded-2xl object-cover h-[30rem] w-[30rem]"
+            />
+            <img 
+              src="spencer-davis-vJsj-hgOEG0-unsplash.jpg" 
+              alt="People sharing a meal on a table"
+              className="mx-2 my-2 rounded-2xl object-cover h-[20rem] w-[20rem]"
+            />
+          </div>
+          <img 
+            src="stefan-vladimirov-Q_Moi2xjieU-unsplash.jpg" 
+            alt="People sharing a meal on a table"
+            className="mx-2 my-2 rounded-2xl object-cover h-[40rem] w-[40rem]" 
+          />
+        </div>
       </div>
     </div>
   );
